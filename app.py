2bd7f103354dfd1e7e0dc3adff074bd2b449da8c
Shubham Yadav <115788094+shubhamyadav1107@users.noreply.github.com>
2024-11-02 14:27:29 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import codecs
+
+# Set page configuration
+st.set_page_config(page_title="Shubham Yadav - Portfolio", layout="wide")
+
+# Load the HTML content
+def load_html(file_path):
+    with codecs.open(file_path, 'r', 'utf-8') as f:
+        html_content = f.read()
+    return html_content
+
+# Display HTML content
+html_file_path = "index.html"
+html_content = load_html(html_file_path)
+st.markdown(html_content, unsafe_allow_html=True)
+
+# Link to CSS
+st.markdown("<style>{}</style>".format(open("style.css").read()), unsafe_allow_html=True)
